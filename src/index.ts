ed317a51815daf0bcb0fa705717d7a5a8dc6d426
Regis <regis@telnyx.com>
2021-01-11 01:51:32 -0300

    A little less hacky way of updating the current note text

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,29 @@ import joplin from 'api';
 import { MenuItemLocation, ToolbarButtonLocation } from 'api/types';
 
 joplin.plugins.register({
-	onStart: async function() {
+  onStart: async function() {
 
     await joplin.commands.register({
-			name: "PrettifyJson",
+      name: "PrettifyJson",
       label: "PrettifyJson",
-			execute: async () => {
-        // runNoteJs();
+      execute: async () => {
         // Get the current note from the workspace.
         const note = await joplin.workspace.selectedNote();
         // Keep in mind that it can be `null` if nothing is currently selected!
         if (note) {
-          // Only run if title matches this :
-          //if (note.title.includes('[json]')){
-            console.info('Note content has changed! New note is:', note);
-            var obj = JSON.parse(note.body);
-            var newBody = JSON.stringify(obj, null, 2);
-            await joplin.data.put(['notes', note.id], null, { body: newBody});
-            await joplin.commands.execute('newNote', note.id);
-            await joplin.commands.execute('openNote', note.id);
-         // }
+          console.info('Note content has changed! New note is:', note);
+          var obj = JSON.parse(note.body);
+          var newBody = JSON.stringify(obj, null, 2);
+          await joplin.data.put(['notes', note.id], null, { body: newBody});
+          await joplin.commands.execute('editor.setText', newBody);
+          await joplin.commands.execute('focusElement', 'noteBody');
         }
         else {
           console.info('No note is selected');
         }
-			}
-		});
+      }
+    });
 
-		await joplin.views.menuItems.create('myMenuItem1', 'PrettifyJson', MenuItemLocation.Tools, { accelerator: 'Ctrl+P' });
-	},
+    await joplin.views.menuItems.create('myMenuItem1', 'PrettifyJson', MenuItemLocation.Tools, { accelerator: 'Ctrl+P' });
+  },
 });
